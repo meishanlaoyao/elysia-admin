2ec0628aa8732649c66630b7eb4db3594265a830
老妖 <1360658549@qq.com>
2026-03-03 14:31:31 +0800

    remove: remove docs

--- docs/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
-
-export default {
-    extends: DefaultTheme,
-    enhanceApp({ app, router, siteData }) {
-        // 可以在这里注册全局组件或添加其他增强功能
-    }
-}
